4bc2623ccf47a0ec1a179c1684974869ceaf8aa8
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-21 00:20:41 -0500

    clean up grapp file

--- graph.py
+++ graph.py
@@ -33,25 +33,11 @@ def plot_file(file):
     plt.ylabel('No Samples')
     print x
     print y
-    t1 = np.arange(10 ** (-5), 100, 10 ** (-4))
 
-    yp = None
-
-    xi = np.linspace(x[0],x[-1],1000)
 
     plt.plot(x, y, 'r--', x,y, 'bo')
 
-    #plt.axis([0, 2*10 **6, 0, 100])
-    #t1 = np.arange(1, 2*10**6, 100)
-
-    #plt.axis([0, 40, -10, 10**6])
-
-
-    #plt.plot( t1,func2(t1), 'b')
-
-    
     plt.show()
-
 
 
 def test():
