24ecfb675a1fb062d8d578f0713fa32a83455884
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-20 19:10:47 -0500

    start work on monte carlo

--- montecarlo.py
+++ montecarlo.py
@@ -1,1 +1,26 @@
-__author__ = 'Jervis'
+from math import exp
+import subprocess
+from time import localtime
+
+__author__ = 'Jervis Muindi'
+
+def hello():
+    print('hello world from idea 9')
+
+def f(x,y,z):
+    # x^2*y + exp(-z)
+    return x*x*y + exp(-z)
+
+def monte_carlo():
+    return None
+
+def main():
+    hello()
+
+
+
+if  __name__ == '__main__':
+    main()
+
+#main()
+
